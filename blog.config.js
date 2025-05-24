6e93455faa327e1a9bac6df44a7134bc14cbddd8
scarlett-s <scarlett-s@users.noreply.github.com>
2025-05-24 10:30:13 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -3,7 +3,7 @@ const BLOG = {
   author: 'Scarlett',
   email: 'scarlett.shu@gmail.com',
   link: 'https://noframes.top',
-  description: 'Made to escape, not to fit.',
+  description: 'made to escape, not to fit.',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'light', // ['light', 'dark', 'auto'],
